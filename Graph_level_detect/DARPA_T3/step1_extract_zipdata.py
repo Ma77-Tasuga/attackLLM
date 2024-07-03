19551302d9b50b3e350d852f8ef1f973ff4fcf27
xu_rui <matt.tasuga@gmail.com>
2024-07-03 18:06:14 +0800

    Try to organize a new form of data.

---
+++ Graph_level_detect/DARPA_T3/step1_extract_zipdata.py
@@ -0,0 +1,8 @@
+import os
+
+os.system('tar -zxvf ./zipdata/ta1-cadets-e3-official.json.tar.gz -C ./extracted_data')
+os.system('tar -zxvf ./zipdata/ta1-cadets-e3-official-2.json.tar.gz -C ./extracted_data')
+os.system('tar -zxvf ./zipdata/ta1-fivedirections-e3-official-2.json.tar.gz -C ./extracted_data')
+os.system('tar -zxvf ./zipdata/ta1-theia-e3-official-1r.json.tar.gz -C ./extracted_data')
+os.system('tar -zxvf ./zipdata/ta1-theia-e3-official-6r.json.tar.gz -C ./extracted_data')
+os.system('tar -zxvf ./zipdata/ta1-trace-e3-official-1.json.tar.gz -C ./extracted_data')
